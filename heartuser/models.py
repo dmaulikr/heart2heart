88ae281a9f9fb63d032cd180f6cab56e3dcbeb2f
esabunor <tesabunor@gmail.com>
2017-09-15 20:08:17 +0800

    fixes

--- heartuser/models.py
+++ heartuser/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 # Create your models here.
 class HeartUser(models.Model):
     heartuser = models.OneToOneField(User)
